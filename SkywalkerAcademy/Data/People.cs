e27915cf86f243474fa4c081a79b5272e4ef8c07
harrdev <jasonharrphx@live.com>
2023-02-04 15:41:02 -0700

    pagination working with bugs on API call

--- SkywalkerAcademy/Data/People.cs
+++ SkywalkerAcademy/Data/People.cs
@@ -1,10 +1,29 @@
 ﻿namespace SkywalkerAcademy.Data
 {
-    public class PeopleClass
+    public class Person    {
+        public string name { get; set; }
+        public string height { get; set; }
+        public string mass { get; set; }
+        public string hair_color { get; set; }
+        public string skin_color { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public string homeworld { get; set; }
+        public List<string> films { get; set; }
+        public List<string> species { get; set; }
+        public List<string> vehicles { get; set; }
+        public List<string> starships { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+    }
+
+    public class PeopleObj
     {
-        public string Birth_year { get; set; }
-        public string Gender { get; set; }
-        public string Name { get; set; }
-        public string Mass { get; set; }
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public List<Person> results { get; set; }
     }
 }
