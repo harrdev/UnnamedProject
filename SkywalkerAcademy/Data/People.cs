9dcb6098f15c341d53f30efee41a34dee5cf7b16
harrdev <jasonharrphx@live.com>
2023-02-04 12:39:05 -0700

    commit

---
+++ SkywalkerAcademy/Data/People.cs
@@ -0,0 +1,10 @@
+﻿namespace SkywalkerAcademy.Data
+{
+    public class PeopleClass
+    {
+        public string Birth_year { get; set; }
+        public string Gender { get; set; }
+        public string Name { get; set; }
+        public string Mass { get; set; }
+    }
+}
